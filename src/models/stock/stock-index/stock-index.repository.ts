fdd42053d21c6d0e94c653b551c6adeb0fba0de5
dwaam <damien.walle@gmail.com>
2023-11-07 22:39:03 +0100

    🔨 (migration): add migration script + do some rework (#13)

--- src/models/stock/stock-index/stock-index.repository.ts
+++ src/models/stock/stock-index/stock-index.repository.ts
@@ -10,7 +10,7 @@ export class StockIndexRepository extends Repository<StockIndex> {
       .insert()
       .into(StockIndex)
       .values(stockIndices)
-      .orUpdate(['ticker', 'name', 'currency'], ['isin'])
+      .orUpdate(['ticker', 'name', 'currency', 'country'], ['isin'])
       .execute();
   }
 }
