395645604ee71e7f1906a77c26cdec50ad175aa6
dwaam <damien.walle@gmail.com>
2023-11-19 16:05:43 +0100

    ♻️ (integration): move asset to stock-integration (#15)

--- src/models/stock/stock-integration/stock-integration.module.ts
+++ src/models/stock/stock-integration/stock-integration.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { AssetController } from '@/models/asset/asset.controller';
-import { StockIntegrationService } from '@/models/asset/stock-integration/stock-integration.service';
+import { StockIntegrationController } from '@/models/stock/stock-integration/stock-integration.controller';
+import { StockIntegrationService } from '@/models/stock/stock-integration/stock-integration.service';
 import { StockTransactionModule } from '@/models/stock/stock-transaction/stock-transaction.module';
 import { DividendModule } from '@/models/stock/dividend/dividend.module';
 import { StockIndexModule } from '@/models/stock/stock-index/stock-index.module';
 
 @Module({
-  controllers: [AssetController],
+  controllers: [StockIntegrationController],
   providers: [StockIntegrationService],
   imports: [StockTransactionModule, DividendModule, StockIndexModule],
 })
-export class AssetModule {}
+export class StockIntegrationModule {}
